f7ccc10171d10446f1e68e3e4bd3e4d4c9ede347
Ahmad Marei <82359651+101ahmadmarei@users.noreply.github.com>
2024-08-03 12:22:16 +0300

    fix: remove unnessary code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,5 @@
-import { changeLanguage } from "./i18n/locales";
-import { useTranslation } from "react-i18next";
-
 function App() {
-  const { t } = useTranslation();
-
-  return (
-    <div>
-      <button onClick={() => changeLanguage("en")}>English</button>
-      <button onClick={() => changeLanguage("ar")}>العربية</button>
-      <h1>{t("welcome")}</h1>
-      <p>{t("description")}</p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;

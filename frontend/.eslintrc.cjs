5067582362851cc39e1c7531534e29e08309a41b
Ahmad Marei <82359651+101ahmadmarei@users.noreply.github.com>
2024-08-03 12:16:44 +0300

    feat: add translation

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,18 +1,68 @@
+/* eslint-env node */
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "@react-native-community",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:storybook/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["simple-import-sort"],
+  root: true,
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "arrow-parens": ["error", "as-needed"],
+    "array-bracket-spacing": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        ignoreRestSiblings: true,
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/ban-ts-comment": "warn",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/member-ordering": "off",
+    "sort-keys": "off",
+    "no-console": "warn",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "react/jsx-curly-brace-presence": [
+      "error",
+      {
+        props: "never",
+        children: "never",
+      },
+    ],
+    "react/jsx-no-bind": "off",
+    "react/jsx-handler-names": [
+      "error",
+      {
+        eventHandlerPrefix: "handleOn",
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "react/self-closing-comp": [
+      "error",
+      {
+        component: true,
+      },
+    ],
+    "react-hooks/exhaustive-deps": "warn",
+    "react-native/no-unused-styles": "error",
+    "react-native/split-platform-components": "error",
+    "react-native/no-color-literals": "off",
+    "react-native/no-raw-text": "off",
+    "react-native/sort-styles": [
+      "error",
+      "asc",
+      {
+        ignoreClassNames: false,
+        ignoreStyleProperties: false,
+      },
     ],
   },
-}
+};
--- frontend/src/i18n/locales.ts
+++ frontend/src/i18n/locales.ts
@@ -2,27 +2,58 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import enTranslation from "./en.json";
 import arTranslation from "./ar.json";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-export const resources = {
-  en: {
-    translation: enTranslation,
-  },
-  ar: {
-    translation: arTranslation,
-  },
+const initializeI18n = () => {
+  const savedLanguage = localStorage.getItem("language") || "en";
+
+  i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init(
+      {
+        lng: savedLanguage,
+        compatibilityJSON: "v3",
+        resources: {
+          ar: { translation: arTranslation },
+          en: { translation: enTranslation },
+        },
+        fallbackLng: "ar",
+        detection: {
+          order: ["querystring", "cookie", "localStorage"],
+          caches: ["localStorage", "cookie"],
+        },
+        keySeparator: ".",
+        missingKeyHandler: function (lng, ns, key, fallbackValue) {
+          console.error(`Missing translation key: ${key}`);
+          throw new Error(`Missing translation key: ${key}`);
+        },
+
+        interpolation: {
+          escapeValue: false,
+        },
+      },
+      () => {
+        updateBodyDirection(i18n.language);
+      }
+    );
+
+  i18n.on("languageChanged", (language) => {
+    updateBodyDirection(language);
+  });
 };
 
-export function initializeI18n(language: string) {
-  i18n.use(initReactI18next).init({
-    resources,
-    lng: language,
-    fallbackLng: "en",
-    interpolation: {
-      escapeValue: false,
-    },
-    returnNull: false,
-    compatibilityJSON: "v3",
-  });
-
-  return i18n;
+function updateBodyDirection(language: string) {
+  if (language === "ar") {
+    document.body.dir = "rtl";
+  } else {
+    document.body.dir = "ltr";
+  }
 }
+
+const changeLanguage = (language: string) => {
+  i18n.changeLanguage(language);
+  localStorage.setItem("language", language); // Save the selected language to localStorage
+};
+
+export { initializeI18n, changeLanguage };

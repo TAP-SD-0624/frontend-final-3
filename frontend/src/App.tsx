b6190674f1628c3bc4ca830ba2a8ff528ed5f3da
Sara Zebdeh <123748808+SaraZebdeh@users.noreply.github.com>
2024-08-06 22:33:44 +0300

    footer and account menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,13 @@
 import Layout from "./components/Layout/Layout";
-import useTheme from "./lib/hooks/useTheme";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./styles/customTheme";
 
 function App() {
-  const [theme, toggleTheme] = useTheme();
-
   return (
     <>
-      <Layout />
-      {/* <div style={{ background: "var(--accent)", color: "var(--dark)" }}>
-        <h1 style={{ color: "var(--primary)" }}>Hello, World!</h1>
-        <p>This is an example component.</p>
-        <button onClick={toggleTheme}>Toggle Theme</button>
-      </div> */}
+      <ThemeProvider theme={theme}>
+        <Layout />
+      </ThemeProvider>
     </>
   );
 }
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import PermIdentityIcon from "@mui/icons-material/PermIdentity";
-import { InputBase, Container, Stack } from "@mui/material";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import ReorderIcon from "@mui/icons-material/Reorder";
+import {
+  InputBase,
+  Container,
+  Stack,
+  Tooltip,
+  MenuItem,
+  Avatar,
+  Divider,
+  ListItemIcon,
+  Menu,
+} from "@mui/material";
 import { NAVBAR_HEIGHT } from "../../constants/index";
 import SearchIcon from "@mui/icons-material/Search";
 import { styled, alpha } from "@mui/material/styles";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
+import { Logout } from "@mui/icons-material";
+import useTheme from "../../lib/hooks/useTheme";
+import LanguageIcon from "@mui/icons-material/Language";
+import { Theme, useMediaQuery } from "@mui/material";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -21,7 +37,6 @@ const Search = styled("div")(({ theme }) => ({
   display: "flex",
   margin: 0,
   width: "362px",
-  // height: "44px",
   padding: theme.spacing(1),
   alignItems: "center",
   justifyContent: "flex-start",
@@ -33,12 +48,10 @@ const Search = styled("div")(({ theme }) => ({
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: "var(--low-emphasis)",
   width: "100%",
-  // height: "18px",
   "& .MuiInputBase-input": {
     fontFamily: "Inter",
     fontSize: "14px",
     fontWeight: 500,
-    // lineHeight: "18px",
     textAlign: "left",
     transition: theme.transitions.create("width"),
   },
@@ -46,7 +59,35 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 const Navbar = () => {
   const navItems = ["Handbags", "Watches", "Skincare", "Jewellery", "Apparels"];
-  // const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [anchorElCetagories, setAnchorElCetagories] =
+    useState<null | HTMLElement>(null);
+
+  const open = Boolean(anchorEl);
+  const openCetagories = Boolean(anchorElCetagories);
+
+  const handleClick = (event: {
+    currentTarget: React.SetStateAction<HTMLElement | null>;
+  }) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseCetagories = () => {
+    setAnchorElCetagories(null);
+  };
+
+  const handleClickCetagories = (event: {
+    currentTarget: React.SetStateAction<HTMLElement | null>;
+  }) => {
+    setAnchorElCetagories(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const [theme, toggleTheme] = useTheme();
+  const isMdUp = useMediaQuery((theme: Theme) => theme.breakpoints.up("md"));
 
   return (
     <AppBar
@@ -77,7 +118,6 @@ const Navbar = () => {
               component="a"
               href="#"
               sx={{
-                display: { xs: "none", md: "flex" },
                 color: "var(--logo)",
                 width: "108px",
                 height: "22px",
@@ -88,31 +128,84 @@ const Navbar = () => {
             >
               CORA'L
             </Typography>
-            <Stack
-              direction="row"
-              sx={{
-                gap: "20px",
-                alignItems: "baseline",
-              }}
-            >
-              {navItems.map((item, index) => (
-                <Typography
-                  key={index}
-                  noWrap
-                  component="a"
-                  href="#"
+            {isMdUp ? (
+              <Stack
+                direction="row"
+                sx={{
+                  gap: "20px",
+                  alignItems: "baseline",
+                }}
+              >
+                {navItems.map((item, index) => (
+                  <Typography
+                    key={index}
+                    noWrap
+                    component="a"
+                    href="#"
+                    sx={{
+                      display: { xs: "none", md: "flex" },
+                      color: "var(--high-emphasis)",
+                      fontFamily: "Inter",
+                      fontSize: "14px",
+                      fontWeight: 500,
+                    }}
+                  >
+                    {item}
+                  </Typography>
+                ))}
+              </Stack>
+            ) : (
+              <>
+                <Tooltip title="Cetagories">
+                  <IconButton
+                    onClick={handleClickCetagories}
+                    size="small"
+                    aria-controls={
+                      openCetagories ? "cetagories-menu" : undefined
+                    }
+                    aria-haspopup="true"
+                    aria-expanded={openCetagories ? "true" : undefined}
+                    sx={{
+                      width: "24px",
+                      height: "24px",
+                    }}
+                  >
+                    <ReorderIcon
+                      sx={{
+                        color: "var(--icon)",
+                      }}
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  anchorEl={anchorElCetagories}
+                  id="cetagories-menu"
+                  open={openCetagories}
+                  onClose={handleCloseCetagories}
                   sx={{
-                    display: { xs: "none", md: "flex" },
-                    color: "var(--high-emphasis)",
-                    fontFamily: "Inter",
-                    fontSize: "14px",
-                    fontWeight: 500,
+                    gap: "10px",
                   }}
                 >
-                  {item}
-                </Typography>
-              ))}
-            </Stack>
+                  {navItems.map((item, index) => (
+                    <MenuItem key={index}>
+                      <Typography
+                        noWrap
+                        component="a"
+                        href="#"
+                        sx={{
+                          color: "var(--high-emphasis)",
+                          fontFamily: "Inter",
+                          fontSize: "14px",
+                          fontWeight: 500,
+                        }}
+                      >
+                        {item}
+                      </Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </>
+            )}
           </Box>
 
           <Box
@@ -123,16 +216,17 @@ const Navbar = () => {
               gap: "20px",
             }}
           >
-            <Search>
-              <SearchIcon
-                sx={{ height: "24px", width: "24px", color: "var(--dark)" }}
-              />
-              <StyledInputBase
-                placeholder="Search for products and brands....."
-                inputProps={{ "aria-label": "search" }}
-              />
-            </Search>
-
+            {isMdUp && (
+              <Search>
+                <SearchIcon
+                  sx={{ height: "24px", width: "24px", color: "var(--dark)" }}
+                />
+                <StyledInputBase
+                  placeholder="Search for products and brands....."
+                  inputProps={{ "aria-label": "search" }}
+                />
+              </Search>
+            )}
             <Stack
               direction="row"
               gap="20px"
@@ -146,13 +240,84 @@ const Navbar = () => {
                   }}
                 />
               </IconButton>
-              <IconButton sx={{ width: "24px", height: "24px" }}>
-                <PermIdentityIcon
+              <Tooltip title="Account settings">
+                <IconButton
+                  onClick={handleClick}
+                  size="small"
+                  aria-controls={open ? "account-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
                   sx={{
-                    color: "var(--icon)",
+                    width: "24px",
+                    height: "24px",
                   }}
-                />
-              </IconButton>
+                >
+                  <PermIdentityIcon
+                    sx={{
+                      color: "var(--icon)",
+                    }}
+                  />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                  elevation: 0,
+                  sx: {
+                    overflow: "visible",
+                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                    mt: 1.5,
+                    "& .MuiAvatar-root": {
+                      width: 32,
+                      height: 32,
+                      ml: -0.5,
+                      mr: 1,
+                    },
+                    "&:before": {
+                      content: '""',
+                      display: "block",
+                      position: "absolute",
+                      top: 0,
+                      right: 14,
+                      width: 10,
+                      height: 10,
+                      bgcolor: "background.paper",
+                      transform: "translateY(-50%) rotate(45deg)",
+                      zIndex: 0,
+                    },
+                  },
+                }}
+                transformOrigin={{ horizontal: "right", vertical: "top" }}
+                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+              >
+                <MenuItem onClick={handleClose}>
+                  <Avatar /> Profile
+                </MenuItem>
+                <MenuItem onClick={toggleTheme}>
+                  <ListItemIcon>
+                    <DarkModeIcon fontSize="small" />
+                  </ListItemIcon>
+                  Switch Theme
+                </MenuItem>
+                <MenuItem>
+                  <ListItemIcon onClick={toggleTheme}>
+                    <LanguageIcon fontSize="small" />
+                  </ListItemIcon>
+                  Switch Language{" "}
+                </MenuItem>
+
+                <Divider />
+                <MenuItem>
+                  <ListItemIcon>
+                    <Logout fontSize="small" />
+                  </ListItemIcon>
+                  Logout
+                </MenuItem>
+              </Menu>
               <IconButton sx={{ width: "24px", height: "24px" }}>
                 <WorkOutlineIcon
                   sx={{

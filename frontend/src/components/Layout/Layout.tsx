9e736db8cb5220bae2db5587d44bb514583bfb09
Sara Zebdeh <123748808+SaraZebdeh@users.noreply.github.com>
2024-08-07 16:39:57 +0300

    feat: sign in and sign up pages

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,12 +1,16 @@
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import Home from "@src/screens/Home";
+import SignIn from "@src/screens/SignIn/SignIn";
+import SignUp from "@src/screens/SignUp";
 
 const Layout = () => {
   return (
     <>
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      {/* <SignIn /> */}
+      <SignUp />
       <Footer />
     </>
   );

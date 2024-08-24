4a9fc6f39185c6142cc617cfd22034bdae8f766f
Sara Zebdeh <123748808+SaraZebdeh@users.noreply.github.com>
2024-08-24 06:17:49 +0300

    protected route

--- frontend/src/routes/ProtectedRoute.tsx
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -1,18 +1,16 @@
-import React, { FC, PropsWithChildren } from 'react'
+import React, { FC } from 'react'
 import useAccountContext from '@src/hooks/useAccountContext';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
 
-const ProtectedRoute: FC<PropsWithChildren> = ({ children }) => {
+const ProtectedRoute: FC = () => {
     const location = useLocation();
     const { getUser } = useAccountContext();
     const user = getUser();
 
     if (!user) return <Navigate to="/login" replace state={{ from: location.pathname }} />;
 
-    if (!children) return <Outlet />
-
-    return (<>{children}</>);
+    return (<Outlet />);
 };
 
 export default ProtectedRoute;
